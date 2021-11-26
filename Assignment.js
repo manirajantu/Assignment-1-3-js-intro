e28e43805e4a264f5f1a523def1cb7f5d849dce0
manirajantu <94602111+manirajantu@users.noreply.github.com>
2021-11-26 18:50:29 +0800

    first commit

---
+++ Assignment.js
@@ -0,0 +1,31 @@
+
+
+console.log(typeof 42);
+console.log(typeof 4.2);
+console.log(typeof "hello");
+console.log(typeof false);
+console.log(typeof NaN);
+console.log(typeof (4 !== 2));
+
+
+console.log( "hamburger" + "s");
+console.log( "hamburgers" - "s");
+console.log( "4" + "2");
+console.log( "4" - "2");
+console.log( "johnny" + 5);
+console.log( "johnny" - 5);
+console.log( 99 * "baloons");
+
+
+
+
+
+let person = 'Mani';
+
+{
+    let person = 'Raja';
+}
+
+
+console.log('My first name is',person)
+
---
+++ assignment-1-3-Mani.js
@@ -0,0 +1,10 @@
+
+const celsius = 37.3;
+
+const celsiusToFahrenheit = ((celsius * 1.8) + 32);
+const celsiusToKelvin = + 273.15;
+
+
+console.log(`Celsius: ${celsius} C`);
+console.log(`Fahrenheit: ${celsiusToFahrenheit} F`);
+console.log(`Kelvin: ${celsiusToKelvin} K`);
